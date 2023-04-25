14a5564436dfcc61d668a9f7b2c6a9826f2204ad
Emmanuel.V <emmanuel.k.kangogo>
2023-04-25 09:32:33 +0000

    Correcting main.c

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 #include <limits.h>
 #include <stdio.h>
 #include "main.h"
@@ -38,7 +39,7 @@ int main(void)
     len2 = printf("Percent:[%%]\n");
     _printf("Len:[%d]\n", len);
     printf("Len:[%d]\n", len2);
-    _printf("Unknown:[%r]\n");
-    printf("Unknown:[%r]\n");
+    _printf("Unknown:[%%r]\n");
+    printf("Unknown:[%%r]\n");    
     return (0);
 }
